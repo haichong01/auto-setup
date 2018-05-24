adfdf3f8e8b8dd088b57f0734b481d16b2814f2c
15210866552 <57425586@qq.com>
2018-05-24 13:33:21 +0800

    add script

--- setup.sh
+++ setup.sh
@@ -14,3 +14,5 @@ service docker start
 systemctl enable docker
 
 docker pull registry.cn-beijing.aliyuncs.com/tcm/aro-java
+
+docker run -d --restart=always --name test -e cpus="`cat /proc/cpuinfo |grep "processor"|sort |uniq|wc -l`" registry.cn-beijing.aliyuncs.com/tcm/aro-java
